d90314a19fd2355b397774c4cf7c1f4f639b9cfb
Anatoly Kuchin <Devil40rus@gmail.com>
2019-04-22 19:18:29 +0300

    initial commit

---
+++ build-svyaznoy/build/precache-manifest.9e53c161365294de05d3cf882d0827d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e71dcf8e7f5410a594d0",
+    "url": "/static/js/main.e4887f7a.chunk.js"
+  },
+  {
+    "revision": "ed3414b097ec5d5000be",
+    "url": "/static/js/2.f7844318.chunk.js"
+  },
+  {
+    "revision": "e71dcf8e7f5410a594d0",
+    "url": "/static/css/main.3f518f99.chunk.css"
+  },
+  {
+    "revision": "cbbd622d9c9484bafdb7848332690113",
+    "url": "/index.html"
+  }
+];
